227c46d99e926109dd5607d6a15f2cae4b00835e
Tor <p.sripayom@gmail.com>
2025-04-04 12:17:34 +0700

    assignment finish

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,6 +1,22 @@
 const moveZeroes = function (nums) {
   //Start coding here
+
+  let left = 0
+  for (let right = 0; right <nums.length; right++){
+    if(nums[right] !== 0){
+      let temp = nums[left]
+      nums[left] = nums[right]
+      nums[right] = temp
+      left++
+      console.log(right)
+      console.log(left)
+      console.log(nums)
+    }
+  }
+  return nums;
 };
+
+// loop
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,6 +1,25 @@
 const romanToInt = function (s) {
+  const romanNum = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
   //Start coding here
+  let total=0;
+  let prevValue=0;
+
+  for ( let i=s.length-1; i>=0; i--){
+    let currentValue = romanNum[s[i]]
+    // console.log(currentValue)
+    if(currentValue<prevValue){
+      total = total - currentValue;
+      // console.log(total);
+    } else {
+      total = total + currentValue;
+      // console.log(total);
+    }
+    prevValue = currentValue
+  }
+  return total;
 };
+
+// if-else loop object
 
 const result1 = romanToInt("III"); // 3
 console.log(result1);
